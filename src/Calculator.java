05863825c17c79e2d532edcd0baeac107ce4ca77
lxve <doeajay12@gmail.com>
2021-10-16 12:14:42 -0400

    Revert "Better error handling and parsing"
    This reverts commit 8bd9345aee09cf72319813adba0186e0fc839af9.

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,56 +8,41 @@ public class Calculator {
         final byte PERCENT = 100;
         final byte MONTHS_IN_YEAR = 12;
         int principal;
-        float annualInterestRate;
-        float monthlyInterestRate;
+        double annualInterestRate;
+        double monthlyInterestRate;
         byte years;
-        short totalPayments;
+        int totalPayments;
 
         while (true) {
-            try {
-                System.out.print("Principal (1,000 - $1,000,000): ");
-                principal = Integer.parseInt(input.nextLine().replaceAll("[$,]", ""));
-                if (principal < 1_000 || principal > 1_000_000) {
-                    System.out.println("Principal must be between $1,000 and $1,000,000");
-                    continue;
-                }
-                break;
+            System.out.print("Principal (1,000 - $1,000,000): ");
+            principal = input.nextInt();
+            if (principal < 1_000 || principal > 1_000_000) {
+                System.out.println("Invalid input");
+                continue;
             }
-            catch (NumberFormatException e) {
-                System.err.println("Did not input an integer");
-            }
+            break;
         }
 
         while (true) {
-            try {
-                System.out.print("Annual Interest Rate (1 - 30%): ");
-                annualInterestRate = Float.parseFloat(input.nextLine().replaceAll("[%]", ""));
-                if (annualInterestRate < 1 || annualInterestRate > 30) {
-                    System.out.println("Interest Rate must be between 1.00% and 30.00%");
-                    continue;
-                }
-                monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
-                break;
+            System.out.print("Annual Interest Rate (1 - 30%): ");
+            annualInterestRate = input.nextDouble();
+            if (annualInterestRate < 1 || annualInterestRate > 30) {
+                System.out.println("Invalid input");
+                continue;
             }
-            catch (NumberFormatException e) {
-                System.err.println("Did not input a number");
-            }
+            monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+            break;
         }
 
         while (true) {
-            try {
-                System.out.print("Years (1 - 30): ");
-                years = Byte.parseByte(input.nextLine());
-                if (years < 1 || years > 30) {
-                    System.out.println("Years must be between 1 and 30 years");
-                    continue;
-                }
-                totalPayments = (short) (years * MONTHS_IN_YEAR);
-                break;
+            System.out.print("Years (1 - 30): ");
+            years = input.nextByte();
+            if (years < 1 || years > 30) {
+                System.out.println("Invalid input");
+                continue;
             }
-            catch (NumberFormatException e) {
-                System.err.println("Did not input an integer");
-            }
+            totalPayments = years * MONTHS_IN_YEAR;
+            break;
         }
 
         double mortgage = principal
